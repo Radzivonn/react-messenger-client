6aa79a43c8ecfbfa554d4cd4a84fcedc0d510d56
Rodion <rodionserez@gmail.com>
2024-05-06 21:23:49 +0300

    feat: add hook for choosing input icons

---
+++ src/hooks/useInputIcon/useInputIcon.tsx
@@ -0,0 +1,30 @@
+import React, { HTMLInputTypeAttribute } from 'react';
+import OpenedEyeIcon from './assets/opened-eye-icon.svg?react';
+import ClosedEyeIcon from './assets/closed-eye-icon.svg?react';
+import SearchIcon from './assets/search-icon.svg?react';
+
+export default function useInputIcon(
+  type: HTMLInputTypeAttribute,
+  isActiveOption: boolean,
+): [React.JSX.Element | null, boolean, HTMLInputTypeAttribute] {
+  let fieldIcon = null;
+  let isInteractive = false;
+  let inputType: HTMLInputTypeAttribute = type;
+
+  switch (type) {
+    case 'password':
+      fieldIcon = isActiveOption ? (
+        <ClosedEyeIcon className="icon" />
+      ) : (
+        <OpenedEyeIcon className="icon" />
+      );
+      inputType = isActiveOption ? 'text' : 'password';
+      isInteractive = true;
+      break;
+    case 'search':
+      fieldIcon = <SearchIcon className="icon" />;
+      break;
+  }
+
+  return [fieldIcon, isInteractive, inputType];
+}
