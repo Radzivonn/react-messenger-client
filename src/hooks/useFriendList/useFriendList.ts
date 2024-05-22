15b70eba1b81d3a387abc7e6ea220496113f8c63
Rodion <rodionserez@gmail.com>
2024-05-22 22:58:41 +0300

    feat: add hooks for convenient work with requests

---
+++ src/hooks/useFriendList/useFriendList.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import UserService from '../../API/services/UserService/UserService';
+
+export const useFriendList = (userId: string) => {
+  return useQuery({
+    queryKey: ['friendList', userId],
+    queryFn: () => UserService.getFriends(userId),
+    staleTime: 60000, // ! test value
+    retry: 1,
+  });
+};
