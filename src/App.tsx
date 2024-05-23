0b4fd33c1628cb9910b9b9774265696b49e47c40
Rodion <rodionserez@gmail.com>
2024-05-23 14:38:40 +0300

    feat: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import './styles/App.scss';
+import { AppRouter } from './router/AppRouter';
 
-function App() {
-  return <></>;
-}
+export const App = () => {
+  return <AppRouter />;
+};
 
 export default App;
