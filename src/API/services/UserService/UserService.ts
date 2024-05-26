65d3760fea62040776f85ed58b76627c6feac07d
Rodion <rodionserez@gmail.com>
2024-05-26 16:21:02 +0300

    feat: add methods to add, remove friend and search users

--- src/API/services/UserService/UserService.ts
+++ src/API/services/UserService/UserService.ts
@@ -15,8 +15,31 @@ export default class UserService {
     }
   }
 
+  public static addFriend(userId: string, friendId: string) {
+    const reqBody = { userId, friendId };
+    const friendsIds: Promise<string[]> = api
+      .post(`user/${endpoints.addFriend}`, { json: reqBody })
+      .json();
+    return friendsIds;
+  }
+
+  public static removeFriend(userId: string, friendId: string) {
+    const reqBody = { userId, friendId };
+    const friendsIds: Promise<string[]> = api
+      .delete(`user/${endpoints.removeFriend}`, { json: reqBody })
+      .json();
+    return friendsIds;
+  }
+
   public static getFriends(userId: string) {
     const friends: Promise<User[]> = api.get(`user/${endpoints.getFriends}/${userId}`).json();
     return friends;
+  }
+
+  public static searchUsers(userId: string, search: string) {
+    const users: Promise<User[]> = api
+      .get(`user/${userId}/${endpoints.searching}/${search}`)
+      .json();
+    return users;
   }
 }
