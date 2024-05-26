df7e853d1d1ba13723b28181e8937fd8eebf16b0
Rodion <rodionserez@gmail.com>
2024-05-26 16:53:42 +0300

    feat: add button to go to search tab

--- src/modules/Main/Sidebar/Sidebar.tsx
+++ src/modules/Main/Sidebar/Sidebar.tsx
@@ -1,19 +1,32 @@
 import React, { ComponentProps, FC } from 'react';
 import './style.scss';
-import { useLocation } from 'react-router-dom';
-import routes from '../../../router/routes';
+import { Link, useLocation } from 'react-router-dom';
+import { mainPageRoutes } from '../../../router/routes';
+import AddFriendIcon from '../../../assets/icons/add-friend-icon.svg?react';
+import { Button } from '../../../components/UI/Button/Button';
 
 export const Sidebar: FC<ComponentProps<'aside'>> = ({ children }) => {
   const { pathname } = useLocation();
   const activeTab = pathname.slice(pathname.lastIndexOf('/') + 1);
-
-  const isAnyTabOpened = (tab: string) => tab === routes.chats || tab === routes.friends;
+  const isFriendsTabOpened = activeTab === mainPageRoutes.friends;
+  const isAnyTabOpened = Object.values(mainPageRoutes).find((route) => route === activeTab);
 
   return (
     <aside className="main-sidebar">
-      <h2 className="main-sidebar__title">
-        {isAnyTabOpened(activeTab) ? activeTab : 'Select any tab please'}
-      </h2>
+      <div className="flex flex-row justify-between">
+        <h2 className="main-sidebar__title">
+          {isAnyTabOpened ? activeTab : 'Select any tab please'}
+        </h2>
+        {isFriendsTabOpened ? (
+          <Link to={mainPageRoutes.searching} className="self-center">
+            <Button accent className="form__button button--icon-only">
+              <AddFriendIcon className="icon" />
+            </Button>
+          </Link>
+        ) : (
+          <></>
+        )}
+      </div>
       <div className="main-sidebar__content">{children}</div>
     </aside>
   );
