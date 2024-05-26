3dcbbe03ce26235c5772eec94053f1da8138e844
Rodion <rodionserez@gmail.com>
2024-05-26 16:27:27 +0300

    feat: add button components to add and remove friends

---
+++ src/components/FunctionalButtons/RemoveFriendButton.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { Button } from '../UI/Button/Button';
+import RemoveFriendIcon from '../../assets/icons/remove-friend-icon.svg?react';
+import { IFriendListActionButtonProps } from './types';
+import { useRemoveFriend } from '../../hooks/useFriendListMutations/useRemoveFriend';
+import { TailSpinner } from '../UI/Spinners/TailSpinner';
+
+export const RemoveFriendButton: FC<IFriendListActionButtonProps> = ({ userId, friendId }) => {
+  const { isPending, mutate } = useRemoveFriend(userId, friendId);
+
+  if (isPending) return <TailSpinner width="30" height="30" wrapperStyle={{ margin: '0' }} />;
+
+  return (
+    <Button accent className="form__button button--icon-only" onClick={() => mutate()}>
+      <RemoveFriendIcon className="icon" />
+    </Button>
+  );
+};
