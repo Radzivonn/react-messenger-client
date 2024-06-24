247497034251aa20446b503723bd3e5bcb0b4dc0
Rodion <rodionserez@gmail.com>
2024-06-24 18:53:59 +0300

    feat: add toast container

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 export const Layout = () => {
   return (
     <>
       <Outlet />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        limit={2}
+        pauseOnHover={false}
+        theme={'dark'}
+        closeOnClick
+        hideProgressBar
+      />
     </>
   );
 };
