ff2423f132e6094d15ab04debefe4800c0c9891d
Rodion <rodionserez@gmail.com>
2024-07-03 23:52:14 +0300

    fix: correct types

--- src/store/socket/socketStore.ts
+++ src/store/socket/socketStore.ts
@@ -1,17 +1,18 @@
 import { create } from 'zustand';
 import { Socket } from 'socket.io-client';
 import { devtools } from 'zustand/middleware';
+import { ServerToClientEvents, ClientToServerEvents } from '../../types/types';
 
 interface SocketStore {
-  socket: Socket | null;
-  setSocket: (socket: Socket) => void;
+  socket: Socket<ServerToClientEvents, ClientToServerEvents> | null;
+  setSocket: (socket: Socket<ServerToClientEvents, ClientToServerEvents>) => void;
   resetSocket: () => void;
 }
 
 export const useSocketStore = create<SocketStore>()(
   devtools((set) => ({
     socket: null,
-    setSocket: (socket: Socket) => set(() => ({ socket })),
+    setSocket: (socket) => set(() => ({ socket })),
     resetSocket: () => set(() => ({ socket: null })),
   })),
 );
