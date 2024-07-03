b3aca20302f52e903a84e48dc6c8ea7935e3605c
Rodion <rodionserez@gmail.com>
2024-07-03 23:58:39 +0300

    feat: update tab components

--- src/components/UI/Tabs/User-tab.tsx
+++ src/components/UI/Tabs/User-tab.tsx
@@ -3,25 +3,32 @@ import { Tab } from './BaseTab/Tab';
 import { RemoveFriendButton } from '../../FunctionalButtons/RemoveFriendButton';
 import { AddFriendButton } from '../../FunctionalButtons/AddFriendButton';
 import { WriteToFriendButton } from '../../FunctionalButtons/WriteToFriendButton';
+import { AvatarPlaceholder } from '../AvatarPlaceholder/AvatarPlaceholder';
 
 interface Props extends ComponentProps<'section'> {
   name: string;
-  isFriend: boolean;
-  userId: string;
   friendId: string;
+  userId: string;
+  isFriend: boolean;
+  isOnline: boolean;
 }
 
-export const UserTab: FC<Props> = ({ name, isFriend, userId, friendId, ...props }) => {
+export const UserTab: FC<Props> = ({ name, friendId, userId, isFriend, isOnline, ...props }) => {
   return (
     <Tab {...props}>
       <div className="flex gap-2 overflow-hidden">
-        <div className="avatar-placeholder">{name.slice(0, 1)}</div>
+        <AvatarPlaceholder name={name} isOnline={isOnline} />
         <h3 className="tab__info_name ml-3">{name}</h3>
       </div>
       <div className="flex gap-2">
         {isFriend ? (
           <>
-            <WriteToFriendButton userId={userId} friendId={friendId} friendName={name} />
+            <WriteToFriendButton
+              userId={userId}
+              friendId={friendId}
+              friendName={name}
+              isOnline={isOnline}
+            />
             <RemoveFriendButton userId={userId} friendId={friendId} friendName={name} />
           </>
         ) : (
