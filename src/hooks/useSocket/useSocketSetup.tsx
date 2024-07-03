b9b7b2e5c35114a4fc6e3336da9b16f8cfca3e0b
Rodion <rodionserez@gmail.com>
2024-07-04 00:46:47 +0300

    feat: rework logic of socket from establishing a new connection for each chat to establishing one single connection after logging into your account

--- src/hooks/useSocket/useSocketSetup.tsx
+++ src/hooks/useSocket/useSocketSetup.tsx
@@ -1,63 +1,57 @@
 import { useEffect } from 'react';
 import { Socket, io } from 'socket.io-client';
-import { Message, WEBSOCKET_EVENTS } from '../../types/types';
-import { ClientToServerEvents, ServerToClientEvents } from './types';
+import { WEBSOCKET_EVENTS } from '../../types/types';
+import { ServerToClientEvents, ClientToServerEvents } from '../../types/types';
 import { useQueryClient } from '@tanstack/react-query';
 import { useSocketStore } from '../../store/socket/socketStore';
-import { useReceiverStore } from '../../store/receiver/receiverStore';
+import { useChatStore } from '../../store/chatData/chatData';
+import { useChangeOnlineStatus } from '../useUserData/useChangeOnlineStatus';
 
-const useSocketSetup = (
-  userId: string,
-  receiverId: string,
-  chatId: string,
-  setMessages: React.Dispatch<React.SetStateAction<Message[]>>,
-) => {
+const useSocketSetup = (userId: string) => {
   const queryClient = useQueryClient();
+  const { mutate: changeOnlineStatus } = useChangeOnlineStatus(userId);
+
   const setSocket = useSocketStore((state) => state.setSocket);
   const resetSocket = useSocketStore((state) => state.resetSocket);
-  const setReceiverStatus = useReceiverStore((state) => state.setReceiverStatus);
+  const { setCurrentChat, addMessage } = useChatStore();
 
   useEffect(() => {
     const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:5050/');
+
+    socket.on(WEBSOCKET_EVENTS.CONNECTION, () => {
+      setSocket(socket);
+      changeOnlineStatus(true);
+    });
 
     socket.on(WEBSOCKET_EVENTS.CONNECTION_ERROR, () => {
       console.log('connection_error');
     });
 
-    socket.on(WEBSOCKET_EVENTS.JOINED_ROOM_SUCCESSFULLY, ({ messages, isCreated }) => {
-      setSocket(socket);
-      setMessages([...messages]);
+    socket.on(WEBSOCKET_EVENTS.JOINED_ROOM_SUCCESSFULLY, ({ chat, isCreated }) => {
+      setCurrentChat(chat);
       if (isCreated) {
         void queryClient.invalidateQueries({ queryKey: ['chatList', userId] });
       }
+      void queryClient.invalidateQueries({ queryKey: ['friendList', userId] });
     });
 
     socket.on(WEBSOCKET_EVENTS.RECEIVE_MESSAGE, (message) => {
-      setMessages((prev) => [...prev, message]);
+      addMessage(message);
       void queryClient.invalidateQueries({ queryKey: ['chatList', userId] });
     });
 
-    socket.on(WEBSOCKET_EVENTS.CONNECT_PARTICIPANT, (isReceiverOnline) => {
-      setReceiverStatus(isReceiverOnline);
-    });
-
-    socket.on(WEBSOCKET_EVENTS.DISCONNECT_PARTICIPANT, () => {
-      setReceiverStatus(false);
-    });
-
     socket.on(WEBSOCKET_EVENTS.DISCONNECT, (reason) => {
+      changeOnlineStatus(false); // ???
       console.log(`Disconnected by reason: ${reason}`);
     });
 
-    socket.emit(WEBSOCKET_EVENTS.JOIN_ROOM, { chatId, userId, receiverId });
-
     return () => {
-      socket.emit(WEBSOCKET_EVENTS.LEAVE_ROOM, chatId);
+      changeOnlineStatus(false);
       resetSocket();
       socket.removeAllListeners();
       socket.close();
     };
-  }, [chatId]);
+  }, []);
 };
 
 export default useSocketSetup;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,27 +1,36 @@
 import React, { ComponentProps, FC } from 'react';
-import { Outlet, useParams, useSearchParams } from 'react-router-dom';
+import './style.scss';
+import { Outlet, useBeforeUnload, useParams, useSearchParams } from 'react-router-dom';
 import { NavigationSidebar } from '../../modules/Main/NavigationSidebar/NavigationSidebar';
 import { Sidebar } from '../../modules/Main/Sidebar/Sidebar';
-import './style.scss';
 import { Chat } from '../../modules/Chat/Chat';
 import { useChatSettingsStore } from '../../store/chatSettings/chatSettingsStore';
 import useWindowResizeHandler from '../../hooks/useWindowResizeHandler/useWindowResizeHandler';
+import useSocketSetup from '../../hooks/useSocket/useSocketSetup';
+import { useSocketStore } from '../../store/socket/socketStore';
+import { useChangeOnlineStatus } from '../../hooks/useUserData/useChangeOnlineStatus';
 
-type userIdSearchParams = { id: string };
+type userSearchParams = { id: string; name: string };
 
 export const Main: FC<ComponentProps<'main'>> = () => {
-  // * id can't be undefined because RequireAuth hoc check id before going to this route
-  const { id } = useParams() as Readonly<userIdSearchParams>;
-
+  // * id and name can't be undefined because RequireAuth hoc check id before going to this route
+  const { id, name } = useParams() as Readonly<userSearchParams>;
   const [searchParams] = useSearchParams();
   const chatId = searchParams.get('chatId');
   const receiverId = searchParams.get('receiverId');
   const receiverName = searchParams.get('receiverName');
+
+  const { mutate: changeOnlineStatus } = useChangeOnlineStatus(id);
+
+  const socket = useSocketStore((state) => state.socket);
   const { isMobile, isChatOpened } = useChatSettingsStore();
-  const isChatActive = !!(chatId && receiverId && receiverName);
+
+  const isChatActive = !!(chatId && receiverId && receiverName && socket && isChatOpened);
   const isSidebarActive = !(isMobile && isChatActive && isChatOpened);
 
-  useWindowResizeHandler(isChatActive);
+  useBeforeUnload(() => changeOnlineStatus(false));
+  useWindowResizeHandler();
+  useSocketSetup(id);
 
   return (
     <main className="main-page">
@@ -29,11 +38,17 @@ export const Main: FC<ComponentProps<'main'>> = () => {
         <NavigationSidebar userId={id} />
         {isSidebarActive && (
           <Sidebar>
-            <Outlet context={{ userId: id }} />
+            <Outlet context={{ userId: id, userName: name }} />
           </Sidebar>
         )}
-        {isChatActive && isChatOpened ? (
-          <Chat userId={id} receiverId={receiverId} receiverName={receiverName} chatId={chatId} />
+        {isChatActive ? (
+          <Chat
+            chatId={chatId}
+            userId={id}
+            userName={name}
+            receiverId={receiverId}
+            receiverName={receiverName}
+          />
         ) : (
           !isMobile && <h2 className="text-hint">Select a chat to start messaging</h2>
         )}
