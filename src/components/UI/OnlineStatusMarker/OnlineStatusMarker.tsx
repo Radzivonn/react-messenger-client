ea49ef1565581ecc562422d7fcfb247395e87d3f
Rodion <rodionserez@gmail.com>
2024-07-04 18:09:56 +0300

    feat: restyle online status marker

--- src/components/UI/OnlineStatusMarker/OnlineStatusMarker.tsx
+++ src/components/UI/OnlineStatusMarker/OnlineStatusMarker.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react';
 
 export const OnlineStatusMarker: FC = () => {
   return (
-    <svg className="relative translate-x-[20%] translate-y-5" height="15">
-      <circle cx="50%" cy="50%" r="6" fill="var(--online-status-marker-color)" />
+    <svg className="relative translate-x-1/3 translate-y-5" height="18">
+      <circle className="online-status-marker" cx="50%" cy="50%" r="7" />
     </svg>
   );
 };
