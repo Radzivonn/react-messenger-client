57ff3be110d49c5edfe866819c2115678a73b19b
Rodion <rodionserez@gmail.com>
2024-08-06 21:59:52 +0300

    feat: add avatar components

---
+++ src/components/UI/AvatarUI/AvatarPlaceholder.tsx
@@ -0,0 +1,17 @@
+import React, { ComponentProps, FC } from 'react';
+import { OnlineStatusMarker } from '../OnlineStatusMarker/OnlineStatusMarker';
+
+interface Props extends ComponentProps<'div'> {
+  name: string;
+  isOnline: boolean;
+}
+
+export const AvatarPlaceholder: FC<Props> = ({ className, name, isOnline, children }) => {
+  return (
+    <div className={`avatar ${className ?? ''}`}>
+      <p className="absolute capitalize">{name.slice(0, 1)}</p>
+      {isOnline && <OnlineStatusMarker />}
+      {children}
+    </div>
+  );
+};
