5ab777af5d7816c6874de72e7868e3d5dd83530e
Rodion <rodionserez@gmail.com>
2024-08-15 22:48:35 +0300

    feat: add adaptivity to emoji picker

--- src/modules/Chat/components/EmojiPicker.tsx
+++ src/modules/Chat/components/EmojiPicker.tsx
@@ -5,6 +5,10 @@ interface Props {
   setInputValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
+const MOBILE_BREAKPOINT = 480;
+const MAX_PER_LINE = 9;
+const MIN_PER_LINE = 6;
+
 export const EmojiPicker: FC<Props> = ({ setInputValue }) => {
   return (
     <div className="absolute bottom-16 right-0 self-end">
@@ -13,6 +17,8 @@ export const EmojiPicker: FC<Props> = ({ setInputValue }) => {
         set="google"
         previewPosition="none"
         navPosition="bottom"
+        perLine={window.innerWidth > MOBILE_BREAKPOINT ? MAX_PER_LINE : MIN_PER_LINE}
+        maxFrequentRows={3}
         onEmojiSelect={(e: { native: string }) => setInputValue((prev) => prev + e.native)}
       />
     </div>
