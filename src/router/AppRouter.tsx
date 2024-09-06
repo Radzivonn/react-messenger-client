b6d35c72046a35b829b48513c4b81e63a525064c
Rodion <rodionserez@gmail.com>
2024-09-06 23:53:04 +0300

    feat: move browser router into app for more flexibility

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Registration } from 'pages/Registration';
 import { Login } from 'pages/Login';
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import { routes } from './routes';
 import { Layout } from './Layout';
 import { Main } from 'pages/Main/Main';
@@ -14,35 +14,33 @@ import { ChatList } from 'modules/Main/ChatList/ChatList';
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={routes.base} element={<Layout />}>
-          <Route index element={<Navigate to={routes.login} replace />} />
-          <Route
-            path={routes.main}
-            element={
-              <RequireAuth>
-                <Main />
-              </RequireAuth>
-            }
-          >
-            <Route path={routes.chats} element={<ChatList />} />
-            <Route path={routes.friends} element={<FriendList />} />
-            <Route path={routes.searching} element={<UserSearch />} />
-          </Route>
-          <Route
-            element={
-              <CheckAuth>
-                <Outlet />
-              </CheckAuth>
-            }
-          >
-            <Route path={routes.registration} element={<Registration />} />
-            <Route path={routes.login} element={<Login />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
+    <Routes>
+      <Route path={routes.base} element={<Layout />}>
+        <Route index element={<Navigate to={routes.login} replace />} />
+        <Route
+          path={routes.main}
+          element={
+            <RequireAuth>
+              <Main />
+            </RequireAuth>
+          }
+        >
+          <Route path={routes.chats} element={<ChatList />} />
+          <Route path={routes.friends} element={<FriendList />} />
+          <Route path={routes.searching} element={<UserSearch />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
+        <Route
+          element={
+            <CheckAuth>
+              <Outlet />
+            </CheckAuth>
+          }
+        >
+          <Route path={routes.registration} element={<Registration />} />
+          <Route path={routes.login} element={<Login />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
