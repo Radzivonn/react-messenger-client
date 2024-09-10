d8800f59deb098047246adf69cc75863eaabc546
Rodion <rodionserez@gmail.com>
2024-09-10 13:57:46 +0300

    test: add zustand lib mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,8 @@ import matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
 
+vi.mock('zustand');
+
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
