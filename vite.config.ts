4413c2624e469a380927d651a8878eaade548eaa
Rodion <rodionserez@gmail.com>
2024-09-20 16:05:42 +0300

    build: update vite and vitest versions and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -20,6 +21,35 @@ export default defineConfig({
       types: '/src/types',
       validations: '/src/validations',
       mocks: '/src/mocks',
+      tests: '/src/tests',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      all: true,
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './tests/unit/coverage',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/test/',
+        'src/tests',
+        'src/mocks',
+        'src/validations/',
+        'src/store/',
+        'src/components/UI/Loaders',
+        'src/components/UI/OnlineStatusMarker',
+        'src/index.tsx',
+        'src/App.tsx',
+        '**/types.ts',
+        '**/*.d.ts',
+        '**/*.test.*',
+        '**/*.spec.*',
+      ],
     },
   },
 });
